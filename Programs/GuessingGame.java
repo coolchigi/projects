6bdc71da793271c48de9efd7c96b9d479e583262
coolchigi <72313512+coolchigi@users.noreply.github.com>
2021-05-12 15:59:59 +0100

    Create GuessingGame.java

--- Programs/GuessingGame.java
+++ Programs/GuessingGame.java
@@ -2,24 +2,32 @@ import java.util.*;
 
 public class GuessingGame {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        myGuessingGame();
+    }
+    
+    public static void myGuessingGame() {
         System.out.println("Welcome to Chigo's version of the guessing game!");
         Random random = new Random();
         int randomNumber = random.nextInt(50);
 
-        while(true){
+        while (true) {
             Scanner userInput = new Scanner(System.in);
-            System.out.println("Enter a number: ");
-            int userNumber = userInput.nextInt();
-            if(userNumber < randomNumber){
-                System.out.println("Your guess is less than the number chosen at random");
-            }else if(userNumber > randomNumber){
-                System.out.println("Your guess is greater than the number chosen at random");
-            }else{
-                System.out.println("You chose the right number");
-                break;
-            }
-        }
+                System.out.println("\nEnter a number: ");
+                int userNumber = userInput.nextInt();
 
+                if (userNumber < randomNumber) {
+                    System.out.printf("%d is less than the number chosen at random",
+                            userNumber);
+                } else if (userNumber > randomNumber) {
+                    System.out.printf("%d is greater than the number chosen at random",
+                            userNumber
+                    );
+                }
+                else {
+                    System.out.printf("%d is the right number! ", userNumber);
+                    break;
+                }
+        }
     }
 }
