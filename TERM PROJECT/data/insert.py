d747ad55cefee71d8ba07f64f5c298681313b904
Chigo <coolchigi0031@gmail.com>
2022-08-25 01:34:04 -0400

    Removed folder from repo

--- TERM PROJECT/data/insert.py
+++
@@ -1,9 +0,0 @@
-import sqlite3;
-
-connect = sqlite3.connect('products_database.db')
-
-connect.execute("INSERT INTO products VALUES(35853359, '30 Vinyl', 10, 'Music', 24.99)")
-connect.execute("INSERT INTO products VALUES(68227675, 'SOUR', 20, 'Music', 15.13)")
-
-connect.commit()
-connect.close()
